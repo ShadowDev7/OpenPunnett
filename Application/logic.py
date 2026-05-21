bfa67ab8ca764c076437c10ad1dea76157b3b620
Shadow <Xristoslazaris21@protonmail.com>
2026-05-21 17:53:02 +0300

    Tha ta ftiaxw meta

--- Application/logic.py
+++ Application/logic.py
@@ -1,24 +1,48 @@
-def calculate_punnett_square(parent1: str, parent2: str) -> list:
+def calculate(parent1: str, parent2: str):
     gametes1 = list(parent1)
     gametes2 = list(parent2)
-    offspring = [] # List to store the possible offspring genotypes (Απόγονοί)
-    
+
+    total = 0
+    result: list[str] = []
+
+    gametes_AA: int = 0
     gametes_aa: int = 0
     gametes_Aa: int = 0
-    gametes_AA: int = 0
-    
-    for g1 in gametes1:
-        for g2 in gametes2:
-            offspring.append(g1 + g2)
-            combination = ''.join(sorted(g1 + g2))
-            if combination == 'aa':
-                gametes_aa += 1
-            elif combination == 'Aa':
-                gametes_Aa += 1
-            elif combination == 'AA':
+
+    for p1 in gametes1:
+        for p2 in gametes2:
+            combination = "".join(sorted(p1 + p2))
+            
+            total += 1
+            result.append(combination)
+
+            if combination == "AA":
                 gametes_AA += 1
-                
+            elif combination == "aa":
+                gametes_aa += 1
+            elif combination == "Aa":
+                gametes_Aa += 1
+
+    phenotype_A: int = gametes_AA + gametes_Aa
+    phenotype_a: int = gametes_aa
+
+    return {
+        "total": total,
+        "combinations": result,
+        "genotype": {
+            "AA": (gametes_AA / total) * 100,
+            "Aa": (gametes_Aa / total) * 100,
+            "aa": (gametes_aa / total) * 100,
+        },
+        "phenotype": {
+            "A": (phenotype_A / total) * 100,
+            "a": (phenotype_a / total) * 100,
+        },
+    }
 
 
+def main():
+    pass 
 
-
+if __name__ == "__main__":
+    main()
--- Application/ui.py
+++ Application/ui.py
@@ -9,8 +9,30 @@ class App(ctk.CTk):
         
         self.grid_columnconfigure(0, weight=1)
         self.label = ctk.CTkLabel(self, text="Welcome to the Punnett Square Calculator!")
-        self.label.grid(row=0, column=0, padx=20, pady=20)
+        self.label.grid(row=0, 
+                        column=0, 
+                        padx=20, 
+                        pady=20
+                        )
+
+        self.button = ctk.CTkButton(self, 
+                                    text="Calculate", 
+                                    command=lambda: self.button.configure(text="Calculating..."),
+                                    height=40,
+                                    width=120,
+                                    font=("Arial", 14),
+                                    text_color="black",
+                                    fg_color="white",
+                                    hover_color="gray",
+                                    corner_radius=20,
+                                    bg_color="white"
+                                )
         
+        self.button.grid(row=1, 
+                         column=0,
+                         pady=20
+                        )
+
 
 if __name__ == "__main__":
     app = App()
